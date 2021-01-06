373a2ff777aa8af7396b7d3c9ba95cb943b35775
Elbert Corniell <elberthcorniell@gmail.com>
2021-01-06 12:10:56 -0400

    title added, and enter to start

--- src/js/scenes/EntryScene.js
+++ src/js/scenes/EntryScene.js
@@ -1,18 +1,28 @@
 import Phaser from 'phaser';
 
 export default class EntryScene extends Phaser.Scene {
-    constructor(){
+    constructor() {
         super('preloader');
     }
 
-    preload(){
+    preload() {
+        this.load.image('title', '/src/images/title.png',);
         this.load.image('tiles', '/src/images/Interiors_free/16x16/Tileset_16x16_1.png');
         this.load.tilemapTiledJSON('room', '/src/map.json');
         this.load.atlas('player', '/src/images/character/texture.png', '/src/images/character/texture.json');
         this.load.atlas('alex', '/src/images/character/alex.png', '/src/images/character/alex.json');
     }
-    
-    create(){
-        this.scene.start('game');
+
+    create() {
+        const image = this.add.image(160, 50, 'title');
+        image.setScale(0.3, 0.3);
+        this.input.keyboard.on('keydown', this.onKeyInput, this);
     }
+
+    onKeyInput({ key }) {
+        if (key === 'Enter')
+            this.scene.start('game');
+    }
+
+
 }
